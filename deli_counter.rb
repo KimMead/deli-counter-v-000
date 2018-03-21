5dda6cadafb5eb4ea51bad94139d58ef3c4b8acd
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-21 23:09:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(array)
     puts "The line is currently empty."
   else array.length > 0
     index = 0
-    message = "The line is currently: "
+    message = "The line is currently:"
     array.each_with_index do |name, index|
-      message += "#{index + 1}. #{name}"
+      message +="#{index + 1}. #{name}"
   end
       puts "#{message}"
   end
