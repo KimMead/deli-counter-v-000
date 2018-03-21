6b5f3f29d59b518e1045efb785b9c0178600c533
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-21 23:07:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else array.length > 0
-    counter = 0
+    index = 0
     message = "The line is currently: "
     array.each_with_index { |name, index| puts message += "#{index + 1}. #{name}" }
       puts "#{message}"
