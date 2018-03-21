86930f62087b0b887ee24dd48c891a40c5e905bf
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-21 23:06:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else array.length > 0
+    counter = 0
+    message = "The line is currently: "
+    array.each_with_index { |name, index| puts message += "#{index + 1}. #{name}" }
+      puts "#{message}"
+    end
+end
+
+line(katz_deli)
