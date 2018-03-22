3ef92c9c923dbf6903bb567bab404abc60d49390
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-22 00:02:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{index + 1} in line."
 end
 
-def now_serving(array, name)
+def now_serving(array)
   if array.empty? == true
     puts "There is nobody waiting to be served!"
   else array.empty? == false
